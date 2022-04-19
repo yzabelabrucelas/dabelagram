e458fa0feed3fd681db8c67b3d156343ab245cc8
yzabelabrucelas <yzabelabrucelas@gmail.com>
2022-04-19 08:38:02 +0800

    Updates

---
+++ precache-manifest.d76b13aded92fea7de528ea55ca602e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2476418a47988a7a46a9fb0f87466d40",
+    "url": "/dabelagram/index.html"
+  },
+  {
+    "revision": "0715e8df3a6579d4d87d",
+    "url": "/dabelagram/static/css/main.a8390e21.chunk.css"
+  },
+  {
+    "revision": "49cea673b40f0ccb83b3",
+    "url": "/dabelagram/static/js/2.eb92d735.chunk.js"
+  },
+  {
+    "revision": "cc1c2cad84108fa4aae7b0d503e9a8a8",
+    "url": "/dabelagram/static/js/2.eb92d735.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0715e8df3a6579d4d87d",
+    "url": "/dabelagram/static/js/main.4f571c98.chunk.js"
+  },
+  {
+    "revision": "9c589d047460c604f8e4",
+    "url": "/dabelagram/static/js/runtime-main.dcca628e.js"
+  }
+]);
