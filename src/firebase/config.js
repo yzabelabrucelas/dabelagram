f49991a68f94f581101d394d00b709f40ad58aef
yzabelabrucelas <yzabelabrucelas@gmail.com>
2022-05-05 10:19:34 +0800

    Deploy ReactJS app to Github

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -10,7 +10,6 @@
 // import { getFirestore } from "firebase/firestore";
 // import { getStorage } from "firebase/storage";
 
-
 // // Your web app's Firebase configuration
 // // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 // const firebaseConfig = {
@@ -30,22 +29,19 @@
 
 // export { projectStorage, projectFirestore };
 
-
-
-
 //v7
 import * as firebase from "firebase/app";
 import "firebase/storage";
 import "firebase/firestore";
 
 var firebaseConfig = {
-  apiKey: "AIzaSyC5CC69-2NRwgL3LCUfKJGKOiTm8H1zxkQ",
-  authDomain: "dabelagram.firebaseapp.com",
-  projectId: "dabelagram",
-  storageBucket: "dabelagram.appspot.com",
-  messagingSenderId: "224796519476",
-  appId: "1:224796519476:web:b0e53ca3dcc61c92e96e61",
-  measurementId: "G-4HELRGM2YV",
+  apiKey: "AIzaSyCN84fke4NmusLAX2d9nRD9U2FdRmAmO7U",
+  authDomain: "dabelagram-5d7a0.firebaseapp.com",
+  projectId: "dabelagram-5d7a0",
+  storageBucket: "dabelagram-5d7a0.appspot.com",
+  messagingSenderId: "682043480320",
+  appId: "1:682043480320:web:e29998a062bbf6bad97be5",
+  measurementId: "G-QQ83GRT8KT"
 };
 
 // Initialize Firebase
